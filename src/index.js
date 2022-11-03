13f1c448e8aee7cd4bbaf0710a9a4e4e82e1da9d
Bidyut Talukdar <bidyut3004@gmail.com>
2022-11-03 15:52:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ require('./db/mongodb')
 const express=require('express')
 const app= express()
 const EmailsRoute=require('./Router/emails')
+const sendEmail=require('./Router/sendEmail')
+
 const port = process.env.PORT || 3000
 
 
 
 app.use(express.json())
 app.use(EmailsRoute)
+app.use(sendEmail)
 
 
 app.listen(port, () => {
